8aa413bb5d063beb90b1ea4eb39633be7031651d
ArtOfCode- <owen.jenkins2903@gmail.com>
2016-05-23 14:59:47 +0100

    add in-chat capability

--- mod/chat-transcript-deleted/chat-transcript-deleted.user.js
+++ mod/chat-transcript-deleted/chat-transcript-deleted.user.js
@@ -2,25 +2,28 @@
 // @name Show deleted chat messages for Stack Exchange
 // @grant none
 // @match *://chat.stackexchange.com/*
+// @match *://chat.stackoverflow.com/*
+// @match *://chat.meta.stackexchange.com/*
 // ==/UserScript==
 
-setTimeout(function() {
+var userscript = function($) {
 
-    var userscript = function($) {
-
-    $('span.deleted').closest('.message').each(function() {
-        var id = this.id.replace('message-', ''), _this = this;
-        $.get('//chat.stackexchange.com/messages/' + id + '/history', function(data) {
-            var msgtxt = data.match(/<div class="content">([\s\S]+?)<\/div>/)[1].trim();
-            $('.content > span', _this).html(msgtxt).css({backgroundColor: '#f4eaea', color: '#000'});
-        });
+    $(document).on("keydown", function(e) {
+        if(e.ctrlKey && e.which == 68) {  // Ctrl-D activates the script
+            e.preventDefault();
+            $('span.deleted').closest('.message').each(function() {
+                var id = this.id.replace('message-', ''), _this = this;
+                $.get('//chat.stackexchange.com/messages/' + id + '/history', function(data) {
+                    var msgtxt = data.match(/<div class="content">([\s\S]+?)<\/div>/)[1].trim();
+                    $('.content > span', _this).html(msgtxt).css({backgroundColor: '#f4eaea', color: '#000'});
+                });
+            });
+        }
     });
 
-    };
+};
 
-    var el = document.createElement('script');
-    el.type = 'text/javascript';
-    el.text = '(' + userscript + ')(jQuery);';
-    document.head.appendChild(el);
-
-}, 2000);
+var el = document.createElement('script');
+el.type = 'text/javascript';
+el.text = '(' + userscript + ')(jQuery);';
+document.head.appendChild(el);
